7743fbd47b255d290c433007ec0a2885217bbf46
ivowiblo <ivowiblo@ubuntu.(none)>
2010-12-27 20:52:05 -0600

    refactor capsula controller

--- app/controllers/CapsulaController.java
+++ app/controllers/CapsulaController.java
@@ -3,27 +3,27 @@ package controllers;
 import java.util.Date;
 import java.util.List;
 
+import models.Capsula;
 import play.mvc.Controller;
 
-public class Capsula extends Controller {
+public class CapsulaController extends Controller {
 
 	public static void displayForm() {
 		render();
 	}
 
 	public static void list() {
-		List<models.Capsula> capsulas = models.Capsula.findAll();
+		List<Capsula> capsulas = Capsula.findAll();
 		render(capsulas);
 	}
 
-	
 	public static void create(String message, Date when) {
-		models.Capsula capsula = new models.Capsula();
+		Capsula capsula = new Capsula();
 		capsula.sendDate = when;
 		capsula.message = message;
-		
+
 		capsula.save();
-		
+
 		render();
 	}
 }
