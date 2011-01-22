50d68c5160f2bbd755b2f1703f83581713847486
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2011-01-22 19:15:40 -0300

    add logging info to arrival notification job

--- app/jobs/ArrivalNotificationJob.java
+++ app/jobs/ArrivalNotificationJob.java
@@ -2,23 +2,26 @@ package jobs;
 
 import java.util.List;
 
-import controllers.CapsulaMailer;
-
 import models.Capsula;
+import play.Logger;
 import play.jobs.Job;
 import play.jobs.On;
+import controllers.CapsulaMailer;
 
 @On("cron.notification.arrival")
 public class ArrivalNotificationJob extends Job {
 
     public void doJob() {
-	List<Capsula> capsulas = Capsula.pendingForNotification();
-
-	for (Capsula capsula : capsulas) {
-	    CapsulaMailer.arrivalNotification(capsula);
-	    capsula.sent = true;
-	    capsula.save();
-	}
+        Logger.info("Starting arrival notification job.");
+        List<Capsula> capsulas = Capsula.pendingForNotification();
+        Logger.info("There are " + capsulas.size() + " notifications to send.");
+        for (Capsula capsula : capsulas) {
+            CapsulaMailer.arrivalNotification(capsula);
+            capsula.sent = true;
+            capsula.save();
+            Logger.info("Sent notification to " + capsula.receiver
+                    + " of capsula #" + capsula.id);
+        }
     }
 
 }
