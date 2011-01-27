824b42180e352c638bcc727518b05fd842fd69f3
Jonathan Leibiusky <ionathan@gmail.com>
2011-01-27 00:11:52 -0300

    add send to a friend feature

--- app/controllers/Capsulas.java
+++ app/controllers/Capsulas.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.util.Date;
-import java.util.List;
 
 import models.Capsula;
 import models.User;
@@ -13,47 +12,55 @@ import play.mvc.Http.StatusCode;
 public class Capsulas extends Controller {
 
     public static void displayForm() {
-        render();
+	render();
     }
 
     @Before(unless = "create")
     static void checkAuthenticated() {
-        if (!Auth.isUserLoggedIn()) {
-            Auth.login();
-        }
+	if (!Auth.isUserLoggedIn()) {
+	    Auth.login();
+	}
     }
 
     public static void created() {
-        render();
+	render();
     }
-    
-    public static void create(String message, Date when) {
-        if (!Auth.isUserLoggedIn()) {
-            forbidden();
-        }
 
-        validation.required(message);
-        validation.required(when);
-        validation.future(when);
+    public static void create(String message, Date when, String receiver) {
+	if (!Auth.isUserLoggedIn()) {
+	    forbidden();
+	}
 
-        if (!validation.hasErrors()) {
-            User user = Auth.getUser();
-            Capsula capsula = new Capsula();
-            capsula.sendDate = when;
-            capsula.message = message;
-            capsula.sender = user;
-            capsula.receiver = user.email;
-            capsula.save();
-            response.status = StatusCode.CREATED;
-            renderJSON(user);
-        } else {
-            badRequest();
-        }
+	validation.required(message);
+	validation.required(when);
+	validation.future(when);
+	if (receiver != null && receiver.length() > 0) {
+	    validation.email(receiver);
+	}
+
+	if (!validation.hasErrors()) {
+	    User user = Auth.getUser();
+	    Capsula capsula = new Capsula();
+	    capsula.sendDate = when;
+	    capsula.message = message;
+	    capsula.sender = user;
+	    if (receiver != null && receiver.length() > 0) {
+		capsula.receiver = receiver;
+	    } else {
+		capsula.receiver = user.email;
+	    }
+	    capsula.save();
+	    response.status = StatusCode.CREATED;
+	    renderJSON(user);
+	} else {
+	    badRequest();
+	}
     }
-    public static void last(){
-    	if (Play.mode.isDev()) {
-    		Capsula lastCapsula = Capsula.last(); 
-    		render(lastCapsula);
-        }
+
+    public static void last() {
+	if (Play.mode.isDev()) {
+	    Capsula lastCapsula = Capsula.last();
+	    render(lastCapsula);
+	}
     }
 }
