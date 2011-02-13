9141f10a058658837d06845934ad155f303bb20b
Jonathan Leibiusky <ionathan@gmail.com>
2011-02-13 14:16:56 -0300

    add robots.txt

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,17 @@
 package controllers;
 
+import java.io.File;
+
 import play.mvc.Controller;
 
 public class Application extends Controller {
     public static void index() {
-        render();
+	render();
+    }
+
+    public static void robots() {
+	File file = play.Play.getFile("public/robots.txt");
+	response.cacheFor("24h");
+	renderBinary(file);
     }
 }
