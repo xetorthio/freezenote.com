ff2d0e8e3895c9a47c43a3b24ee44c24c14748f1
Jonathan Leibiusky <ionathan@gmail.com>
2011-07-02 12:50:41 -0300

    add testing e-mail

--- app/jobs/Bootstrap.java
+++ app/jobs/Bootstrap.java
@@ -10,16 +10,19 @@ import play.test.Fixtures;
 @OnApplicationStart
 public class Bootstrap extends Job {
     public void doJob() {
-	if(Play.mode.isDev()) {
+	if (Play.mode.isDev()) {
 	    Fixtures.deleteAllModels();
 	    Fixtures.loadModels("dev-data.yaml");
 	}
-        Properties props = play.libs.Mail.getSession().getProperties();
-        props.setProperty("mail.transport.protocol.rfc822", Play.configuration
-                .getProperty("mail.transport.protocol.rfc822", "smtp"));
-        props.setProperty("mail.aws.user", Play.configuration.getProperty(
-                "mail.aws.user", ""));
-        props.setProperty("mail.aws.password", Play.configuration.getProperty(
-                "mail.aws.password", ""));
+	if (!Play.mode.isDev()) {
+	    Properties props = play.libs.Mail.getSession().getProperties();
+	    props.setProperty("mail.transport.protocol.rfc822",
+		    Play.configuration.getProperty(
+			    "mail.transport.protocol.rfc822", "smtp"));
+	    props.setProperty("mail.aws.user",
+		    Play.configuration.getProperty("mail.aws.user", ""));
+	    props.setProperty("mail.aws.password",
+		    Play.configuration.getProperty("mail.aws.password", ""));
+	}
     }
 }
