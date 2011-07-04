be8021f2e2968bdcf20e55c45a79d05e9cd8aa07
Jonathan Leibiusky <ionathan@gmail.com>
2011-07-04 19:09:42 -0300

    add logging to notification

--- app/controllers/NotesMailer.java
+++ app/controllers/NotesMailer.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.Note;
 import models.Receiver;
+import play.Logger;
 import play.Play;
 import play.i18n.Lang;
 import play.i18n.Messages;
@@ -10,6 +11,8 @@ import play.mvc.Router;
 
 public class NotesMailer extends Mailer {
     public static void arrivalNotification(Note note, Receiver receiver) {
+	Logger.info("Sending e-mail notificaion of note " + note.id
+		+ " to receiver " + receiver.id);
 	Lang.change(note.sender.language);
 	setFrom(Play.configuration.getProperty("mail.from"));
 	setSubject(Messages.get("mail.arrival.subject"));
